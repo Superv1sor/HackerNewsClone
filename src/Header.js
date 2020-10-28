c31e80cc186d62a58e0bb884e9fcaba3e4dc4e4c
Ana Caballero <ana.caballero@gmx.de>
2020-10-28 08:18:20 +0100

    add search feature

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,6 @@
-export default function Header({q}) {
+import SearchForm from "./SearchForm";
+
+export default function Header({setQuery}) {
     return (
     <header className="SearchHeader container">
         <div className="SearchHeader_search">
@@ -8,7 +10,7 @@ export default function Header({q}) {
             </a>
             <div className="SearchHeader_container">
                 <span className="SearchIcon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></span>
-                <input type="search" placeholder="Search stories by title, url or author" className="SearchInput" value={q}></input>
+                <SearchForm setQuery={setQuery} />
             </div>
         </div>
     </header>
