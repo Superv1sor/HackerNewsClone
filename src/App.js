df9b1563616e916a5d75d281cbdde7fe18771871
Ana Caballero <ana.caballero@gmx.de>
2020-10-28 17:59:29 +0100

    add loading spinner, no search results message, error handling, data refresh every 5 minutes

--- src/App.js
+++ src/App.js
@@ -6,29 +6,53 @@ import './App.css';
 function App() {
   const [articles, setArticles] = useState([]);
   const [query, setQuery] = useState("react");
+  const [loading, setLoading] = useState(false);
+ 
+  const fetchData = (setArticles) => {
+    setLoading(true)
+    let endpoint = `https://hn.algolia.com/api/v1/search?query=${query}`
+    fetch(endpoint)
+    .then((response) => response.json())
+    .then((response) => {
+        setLoading(false);
+        const newArticles = response.hits.map((result) => ({
+            text: result.title,
+            url: result.url,
+            num_comments: result.num_comments,
+            isCompleted: false,
+        }))
+      .sort((a, b) => (a.num_comments > b.num_comments ? -1:1));
+      setArticles(newArticles);
+      setQuery(response.query);
+    })
+    // Error handling
+    .catch(error => {
+      setLoading(false);
+      alert(error);
+    });
+  }
 
   useEffect(() => {
-      let endpoint = `https://hn.algolia.com/api/v1/search?query=${query}`
-      fetch(endpoint)
-      .then((response) => response.json())
-      .then((response) => {
-          const newArticles = response.hits.map((result) => ({
-              text: result.title,
-              url: result.url,
-              num_comments: result.num_comments,
-              isCompleted: false,
-          }))
-        .sort((a, b) => (a.num_comments > b.num_comments ? -1:1));
-        setArticles(newArticles);
-        setQuery(response.query);
-      });
+      fetchData(setArticles);
+      // Automatic data refresh after 5 minutes
+      const interval = setInterval(() => {
+        fetchData(setArticles);
+      }, 300000);
+      return () => clearInterval(interval);
   }, [query]);
+
 
   return (
     <div className="app">
       <Header setQuery={setQuery} />
-      <div className="todo-list">
-        <div className="search-term">News about "{query}":</div>
+      {/*Display spinner if news are loading*/}
+      <div className="loader-container" style={loading ? {display:"block"} : {display:"none"}}> 
+        <div className="loader"></div>
+      </div>
+      <div className="todo-list" style={loading ? {display:"none"} : {display:"block"}}>
+
+    {/*Check if search gave results*/}
+  <div className="search-term">{articles.length ? `News about "${query}": ` : `No news found for "${query}"`}</div>
         {articles.map((article, index) => (
           <Article
             key={index}
