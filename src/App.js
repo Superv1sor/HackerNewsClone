c9094dbb7cf7c75080e41259fe01a55a8fd4cb2a
Heiko <hfrosch@gmail.com>
2020-10-30 13:18:08 +0100

    add pagination
    add dark mode

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Article from "./Article.js";
 import Header from "./Header.js";
 import './App.css';
-//import './index.css';
-//import './style2.css';
+import ReactPaginate from 'react-paginate';
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [query, setQuery] = useState("react");
   const [loading, setLoading] = useState(false);
-  // const [stylePath, setStylePath] = useState("./index.css");
- 
-  const fetchData = (setArticles) => {
-    setLoading(true)
-    let endpoint = `https://hn.algolia.com/api/v1/search?query=${query}`
+  const [pageCount, setPageCount] = useState(50); // api support up to a maximum of 50 page only
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const fetchData = () => {
+    setLoading(true);
+    let endpoint = `https://hn.algolia.com/api/v1/search?query=${query}&page=${currentPage}`;
     fetch(endpoint)
     .then((response) => response.json())
     .then((response) => {
@@ -30,6 +30,7 @@ function App() {
       .sort((a, b) => (a.num_comments > b.num_comments ? -1:1));
       setArticles(newArticles);
       setQuery(response.query);
+      setPageCount(response.nbPages);
     })
     // Error handling
     .catch(error => {
@@ -38,18 +39,22 @@ function App() {
     });
   }
 
+  const pageChange = (data) => {
+    setCurrentPage(data.selected);
+    fetchData();
+  }
+
   useEffect(() => {
-      fetchData(setArticles);
+      fetchData();
       // Automatic data refresh after 5 minutes
       const interval = setInterval(() => {
-        fetchData(setArticles);
+        fetchData();
       }, 300000);
       return () => clearInterval(interval);
   }, [query]);
 
    return (
     <div className="default light">
-     
       <div className="container">
         <Header setQuery={setQuery} />
         {/*Display spinner if news are loading*/}
@@ -69,7 +74,17 @@ function App() {
             ))}
           </div>
         </section>
+        <div className="pagination">
+       <ReactPaginate
+         pageCount={pageCount}
+         marginPagesDisplayed={2}
+         pageRangeDisplayed={5}
+         onPageChange={pageChange}
+       />
+       </div>
+
       </div>
+
     </div>
   );
 }
